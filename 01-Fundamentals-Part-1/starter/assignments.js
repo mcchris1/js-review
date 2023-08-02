fc6bcddb67f7c98535d1d5a42c954a1b860faeee
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-08-01 20:34:07 -0400

    adds fundamentals pt 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,121 @@
+// const country = 'US';
+// const continent = 'North Am';
+// let population = 300000000; 
+// let populationFin = 6000000;
+// let populationAvg = 33000000;
+// const isIsland = false;
+// let language;
+// language = "English";
+
+// const description = country+" is in "+continent+", and its "+population+" people speak "+language
+// const description = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+// console.log(language, isIsland)
+
+// console.log(population / 2);
+// population+=1;
+// console.log(population)
+
+// console.log(population > populationFin);
+// console.log(population < populationAvg);
+
+// console.log(description);
+
+// let massMark = 95;
+// let heightMark = 1.88;
+// let massJohn = 85;
+// let heightJohn = 1.76;
+
+// const BMIMark = massMark/heightMark**2;
+// const BMIJohn = massJohn/(heightJohn*heightJohn);
+// const BMIComp = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn);
+// console.log(BMIComp);
+
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's BMI ${BMIMark} is higher than John's!`)
+// }else {
+//     console.log(`John's BMI ${BMIJohn} is higher than Mark's!`)
+// }
+
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = "I'm "+firstName +', a '+(year-birthYear)+' year old '+job+"!";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}`;
+// console.log(jonasNew);
+
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(inputYear + 18);
+
+// const favorite = Number(prompt("What's your favorite number?"));
+// console.log(favorite);
+// console.timeLog(typeof favorite);
+
+// if (favorite === 23) {
+//     console.log("Cool! 23 is an amazing number!")
+// } else if (favorite === 7) {
+//     console.log("7 is pretty tight")
+// } else if (favorite === 9) {
+//     console.log("9 is pretty tight")
+// } else {
+//     console.log("All is lost")
+// }
+
+// if(favorite !== 23) console.log("Why not 23?");
+
+// avgScoreDolphins = (96+108+89)/3
+// avgScoreKoalas = (88+91+110)/3
+
+// if (avgScoreDolphins > avgScoreKoalas) {
+//     console.log("Dolphins better")
+// } else if (avgScoreKoalas > avgScoreDolphins) {
+//     console.log("Koalas better")
+// } else {console.log("y'all's both avg")}
+
+// const day = 'turday';
+
+// switch(day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log ('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log("Enjoy the weekend");
+//         break;
+//     default:
+//         console.log("Not a valid day")
+// }
+
+// const age = 23;
+// // age >= ? console.log("I like to drink wine") : console.log('I like to drink water');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+// const bill = 275;
+// const tip = bill > 50 && bill < 300 ? Number(bill)*.15 : Number(bill)*.2;
+// console.log(String(bill), String(tip), String(bill+tip));
